92b1bd7129ca0a51e8865da98dcc1ffa0f1a7a9d
Benedikt Müller <d12bb@posteo.de>
2022-04-19 16:45:12 +0200

    [fish] auth sudo with Touch ID

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,12 @@ end
 source (brew --prefix asdf)/libexec/asdf.fish
 fish_add_path $CARGO_HOME/bin
 fish_add_path $HOME/.local/bin
+
+# Auth sudo with Touch ID
+# Gets deleted with every system update or so
+if not grep pam_tid /etc/pam.d/sudo > /dev/null 2>&1
+	echo 'Adding Touch ID to sudo again...'
+	echo "sudo sed -i '.bak' '/sufficient/p; s/smartcard/tid/' /etc/pam.d/sudo"
+	sudo sed -i '.bak' '/sufficient/p; s/smartcard/tid/' /etc/pam.d/sudo
+end
+
