60796f8ad58e4ca1d8cca78fcffb3580eee9c7f1
Benedikt Müller <d12bb@posteo.de>
2022-04-19 13:30:28 +0200

    [nvim] nicer markdown preview

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,19 +1,25 @@
 -- Open Markdown preview (via glow) in floating window, exit with <esc>
--- TODO: make window nicer, not 100% width/height
 local preview = function()
-	local path = vim.fn.shellescape(vim.fn.expand('%'))
-	local glowbuf = vim.api.nvim_create_buf(false, true)
-	local glowwin = vim.api.nvim_open_win(glowbuf, true, {
+	local path   = vim.fn.shellescape(vim.fn.expand('%'))
+	local pbuf   = vim.api.nvim_create_buf(false, true)
+	local height = vim.api.nvim_list_uis()[1].height
+	local width  = vim.api.nvim_list_uis()[1].width
+
+	vim.api.nvim_open_win(pbuf, true, {
 		relative = 'editor',
-		width    = 999,
-		height   = 999,
-		row      = 0,
-		col      = 0,
+		width    = math.floor(width * 0.8),
+		height   = math.floor(height * 0.8),
+		row      = (height/2) - (height*0.4),
+		col      = (width/2) - (width*0.4),
+		style    = 'minimal',
+		border   = 'rounded',
 	})
-	vim.api.nvim_buf_set_keymap(glowbuf, 'n', '<esc>', ':q<cr>', {})
+	vim.api.nvim_buf_set_keymap(pbuf, 'n', '<esc>', ':q<cr>', {})
 	vim.fn.termopen('glow '..path)
 end
 require('which-key').register {['<leader>p'] = { preview, 'Preview Markdown' }}
+-- vim.keymap.set('n', '<leader>p', preview, { desc = 'Preview Markdown' })
+--   as soon as WhichKey implements desc
 
 if vim.fn.findfile("Makefile") == "" then
 	vim.cmd ':compiler pandoc'
