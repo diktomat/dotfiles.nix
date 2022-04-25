1d1ae896e3cc8b5c02019b82f9817181727d3d21
Benedikt Müller <d12bb@posteo.de>
2022-04-25 17:28:58 +0200

    fix[nvim]: don't create autocmds multiple times

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -1,6 +1,9 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		vim.lsp.buf.formatting()
-	end,
-})
+if vim.g.go_autocmd == nil then
+	vim.g.go_autocmd = true
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = function()
+			vim.lsp.buf.formatting()
+		end,
+	})
+end
--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,6 +1,9 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.lua",
-	callback = function()
-		vim.cmd("%!stylua --stdin-filepath % -")
-	end,
-})
+if vim.g.lua_autoformat == nil then
+	vim.g.lua_autoformat = true
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.lua",
+		callback = function()
+			vim.cmd("%!stylua --stdin-filepath % -")
+		end,
+	})
+end
