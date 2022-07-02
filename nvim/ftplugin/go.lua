431568b7e754476ae494e2e015ac37ea0ca56c6d
Benedikt Müller <d12bb@posteo.de>
2022-07-02 19:57:33 +0200

    [nvim] format Go on save

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -1,2 +1,9 @@
 vim.bo.makeprg = "go build 2>&1"
 vim.bo.errorformat = "%f:%l:%c: %m,%-G#%s"
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = "d12bb",
+	pattern = "*.go",
+	desc = "Format on save",
+	callback = vim.lsp.buf.formatting_sync,
+})
