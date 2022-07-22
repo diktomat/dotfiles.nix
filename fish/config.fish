1c093557f501a5a5e578c9b56c8cd07bddb8e873
Benedikt Müller <d12bb@posteo.de>
2022-07-22 20:17:04 +0200

    [fish] more fzf

--- fish/config.fish
+++ fish/config.fish
@@ -22,6 +22,9 @@ if not grep pam_tid /etc/pam.d/sudo > /dev/null 2>&1
 end
 
 # FZF Config
-set -gx FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --color always"
-set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND . \$dir"
-set -gx FZF_DEFAULT_OPTS "--ansi --tabstop=4 --preview='bat --color=always {}'"
+set -gx FZF_DEFAULT_COMMAND "fd --hidden --color always"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
+set -gx FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND --type directory"
+set -gx FZF_DEFAULT_OPTS "--ansi --tabstop=4 --preview='bat -p --color=always {}'"
+set -gx FZF_CTRL_R_OPTS "--preview 'echo {} |bat -l fish -p --color=always' --preview-window down:3:wrap"
+set -gx FZF_ALT_C_OPTS "--preview 'lsd --color always --icon always {}'"
