b7b58b6d0c39b49c56b9890692f50735707dfa33
Benedikt Müller <d12bb@posteo.de>
2022-07-23 15:53:56 +0200

    [fish] more fzf

--- fish/config.fish
+++ fish/config.fish
@@ -25,6 +25,6 @@ end
 set -gx FZF_DEFAULT_COMMAND "fd --hidden --color always"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
 set -gx FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND --type directory"
-set -gx FZF_DEFAULT_OPTS "--ansi --tabstop=4 --preview='bat -p --color=always {}'"
-set -gx FZF_CTRL_R_OPTS "--preview 'echo {} |bat -l fish -p --color=always' --preview-window down:3:wrap"
+set -gx FZF_DEFAULT_OPTS "--ansi --tabstop=4 --preview='bat -p --color=always {}' --info=inline --tiebreak=length"
+set -gx FZF_CTRL_R_OPTS "--preview 'echo {} |bat -l fish -p --color=always' --preview-window=down:3:wrap --reverse"
 set -gx FZF_ALT_C_OPTS "--preview 'lsd --color always --icon always {}'"
---
+++ fish/functions/fbi.fish
@@ -0,0 +1,10 @@
+function fbi --description 'Install Brew package via FZF'
+	set -f PREVIEW 'HOMEBREW_COLOR=1 brew info {}'
+	set -f PKGS (brew formulae) (brew casks)
+	set -f INSTALL_PKGS (echo $PKGS |sed 's/ /\n/g' |fzf -m --preview=$PREVIEW --tiebreak=length)
+	if test ! -z "$INSTALL_PKGS"
+		brew install $INSTALL_PKGS
+	else
+		echo "Nothing to install…"
+	end
+end
