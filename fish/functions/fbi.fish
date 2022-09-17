5734ceb123e6c57d227a60397dd4cfb864655632
Benedikt Müller <d12bb@posteo.de>
2022-09-17 11:22:29 +0200

    [fish] fbi

--- fish/functions/fbi.fish
+++ fish/functions/fbi.fish
@@ -1,7 +1,7 @@
-function fbi --description 'Install Brew package via FZF'
+function fbi -a query -d 'Install Brew package via FZF'
 	set -f PREVIEW 'HOMEBREW_COLOR=1 brew info {}'
 	set -f PKGS (brew formulae) (brew casks)
-	set -f INSTALL_PKGS (echo $PKGS |sed 's/ /\n/g' |fzf -m --preview=$PREVIEW --tiebreak=length)
+	set -f INSTALL_PKGS (echo $PKGS |sed 's/ /\n/g' |fzf -m --preview=$PREVIEW --query=$query --tiebreak=length)
 	if test ! -z "$INSTALL_PKGS"
 		brew install $INSTALL_PKGS
 	else
