4f6c9d4065ad7ba597a075745cbd4c5b9b3d5f6d
Benedikt Müller <d12bb@posteo.de>
2022-10-23 14:45:13 +0200

    [fish] Better casks in fbi()

--- fish/functions/fbi.fish
+++ fish/functions/fbi.fish
@@ -1,7 +1,11 @@
 function fbi -a query -d 'Install Brew package via FZF'
 	set -f PREVIEW 'HOMEBREW_COLOR=1 brew info {}'
-	set -f PKGS (brew formulae) (brew casks)
-	set -f INSTALL_PKGS (echo $PKGS |sed 's/ /\n/g' |fzf -m --preview=$PREVIEW --query=$query --tiebreak=length)
+	set -f PKGS (brew formulae) (brew casks |sed 's|^|homebrew/cask/|')
+
+	set -f INSTALL_PKGS (echo $PKGS \
+		|sed 's/ /\n/g' \
+		|fzf --multi --preview=$PREVIEW --query=$query --nth=-1 --with-nth=-2.. --delimiter=/)
+
 	if test ! -z "$INSTALL_PKGS"
 		brew install $INSTALL_PKGS
 	else
