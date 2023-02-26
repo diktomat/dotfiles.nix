6098387f8e100dfc82236312a431902682fd0f44
Benedikt Müller <d12bb@posteo.de>
2023-02-26 12:49:15 +0100

    [nix-conversion] macos defaults

--- macos.nix
+++ macos.nix
@@ -13,11 +13,6 @@
 			extra-nix-path = nixpkgs=flake:nixpkgs
 		'';
 	};
-	programs.fish = {
-		enable = true;
-		useBabelfish = true;
-		babelfishPackage = pkgs.babelfish;
-	};
 	environment = {
 		shells = [pkgs.fish];
 		loginShell = pkgs.fish;
@@ -32,10 +27,38 @@
 		masApps = {};
 		# onActivation.cleanup = "uninstall";
 	};
+	programs.fish = {
+		enable = true;
+		useBabelfish = true;
+		babelfishPackage = pkgs.babelfish;
+	};
+	system.defaults = {
+		dock.autohide = true;
+		dock.autohide-delay = 0.0;
+		finder.AppleShowAllExtensions = true;
+		finder.AppleShowAllFiles = true;
+		finder.FXEnableExtensionChangeWarning = false;
+		finder.FXPreferredViewStyle = "clmv"; # column view by default
+		finder.ShowPathbar = true;
+		finder.ShowStatusBar = true;
+		NSGlobalDomain."com.apple.keyboard.fnState" = true; # Fn keys are F1-12
+		NSGlobalDomain.AppleKeyboardUIMode = 3; # full keyboard control of dialogs
+		NSGlobalDomain.AppleShowAllExtensions = true;
+		NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+		NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
+		screencapture.disable-shadow = true;
+		screencapture.location = "$HOME/Desktop";
+		screencapture.type = "png";
+	};
+	system.defaults.CustomSystemPreferences = {
+		"com.apple.TextEdit".RichText = 0;
+	};
+	system.keyboard.enableKeyMapping = true;
+	system.keyboard.remapCapsLockToEscape = true;
+	system.stateVersion = 4;
+	security.pam.enableSudoTouchIdAuth = true;
 	users.users.bene = {
 		home = "/Users/bene";
 		shell = pkgs.fish;
 	};
-	system.stateVersion = 4;
-	# security.pam.enableSudoTouchIdAuth = true;
 }
